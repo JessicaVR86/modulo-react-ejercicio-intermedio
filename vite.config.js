b90a191900aeda2dacb460e28158e045d5b89678
JessicaVR86 <jessica.vld.rdi@gmail.com>
2025-06-08 10:18:05 +0200

    names

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://jessicavr86.github.io/modulo-react-ejercicio-intermedio/",
   plugins: [react()],
   server: {
     open: "/",
